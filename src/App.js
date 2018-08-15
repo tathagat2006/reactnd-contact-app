5432cd5b3c98b6e8c69498266c01ab86978c0e92
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-08-15 20:02:37 +0530

    contact list component created
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+class ContactList extends React.Component {
+
+    render() {
+
+        const people  = [
+            {name: 'Tathagat'},
+            {name: 'Tanisha'},
+            {name: 'Tanuj'}
+        ]
+
+        return <ol>
+            {people.map((person) => {
+                return <li key={person.name}>{person.name}</li>
+            })}
+        </ol>
+    }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContactList />
       </div>
     );
   }
