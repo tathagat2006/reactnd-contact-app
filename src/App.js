163d66c7250e8f040205ef1896a342cb5dff06db
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-08-15 22:34:50 +0530

    ListContacts component added
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <p>Hi</p>
+        <ListContacts contacts={contacts}/>
       </div>
     )
   }
---
+++ src/ListContacts.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react'
+
+class ListContacts extends Component {
+
+    render() {
+        return (
+            <ol className='contact-list'>
+
+            </ol>
+        )
+    }
+}
+
+export default ListContacts;
