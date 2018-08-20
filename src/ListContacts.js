07dc249c573694b72df2195fb5c0eeca57112e6a
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-08-20 22:31:00 +0530

    stateless functional component used for ListConatact class
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,11 +1,11 @@
 import React , {Component} from 'react'
+//you can ignore the above line as you are using the stateless functional component.
 
-class ListContacts extends Component {
-
-    render() {
-        return (
-            <ol className='contact-list'>
-                {this.props.contacts.map((contact) => (
+//you can also make function instead of using class's render method
+function ListContacts(props) {
+    return(
+        <ol className='contact-list'>
+                {props.contacts.map((contact) => (
                     <li key={contact.id} className='contact-list-item'>
                         <div className= 'contact-avatar'
                             style={
@@ -25,8 +25,36 @@ class ListContacts extends Component {
                     </li>
                 ))}
             </ol>
-        )
-    }
+    )
 }
+
+// class ListContacts extends Component {
+//
+//     render() {
+//         return (
+//             <ol className='contact-list'>
+//                 {this.props.contacts.map((contact) => (
+//                     <li key={contact.id} className='contact-list-item'>
+//                         <div className= 'contact-avatar'
+//                             style={
+//                                 {
+//                                     backgroundImage: `url(${contact.avatarURL})`
+//                                 }
+//                             }
+//                         ></div>
+//
+//                         <div className='contact-details'>
+//                             <p>{contact.name}</p>
+//                             <p>{contact.handle}</p>
+//                         </div>
+//                         <button className= 'contact-remove'>
+//                             Remove
+//                         </button>
+//                     </li>
+//                 ))}
+//             </ol>
+//         )
+//     }
+// }
 
 export default ListContacts;
