623d4ab40b99c5bda304fa66fd5b794fe4cfb5b2
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-08-22 00:51:44 +0530

    stateless function component to class component in Listcomponent.js
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -4,43 +4,10 @@ import React , {Component} from 'react'
 import PropTypes from 'prop-types';
 
 //you can also make function instead of using class's render method
-function ListContacts(props) {
-    return(
-        <ol className='contact-list'>
-                {props.contacts.map((contact) => (
-                    <li key={contact.id} className='contact-list-item'>
-                        <div className= 'contact-avatar'
-                            style={
-                                {
-                                    backgroundImage: `url(${contact.avatarURL})`
-                                }
-                            }
-                        ></div>
-
-                        <div className='contact-details'>
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>
-                        </div>
-                        <button onClick={() => props.onDeleteContact(contact)} className= 'contact-remove'>
-                            Remove
-                        </button>
-                    </li>
-                ))}
-            </ol>
-    )
-}
-
-ListContacts.protoTypes = {
-    contacts: PropTypes.array.isRequired,
-    onDeleteContact: PropTypes.func.isRequired
-}
-
-// class ListContacts extends Component {
-//
-//     render() {
-//         return (
-//             <ol className='contact-list'>
-//                 {this.props.contacts.map((contact) => (
+// function ListContacts(props) {
+//     return(
+//         <ol className='contact-list'>
+//                 {props.contacts.map((contact) => (
 //                     <li key={contact.id} className='contact-list-item'>
 //                         <div className= 'contact-avatar'
 //                             style={
@@ -54,14 +21,54 @@ ListContacts.protoTypes = {
 //                             <p>{contact.name}</p>
 //                             <p>{contact.handle}</p>
 //                         </div>
-//                         <button className= 'contact-remove'>
+//                         <button onClick={() => props.onDeleteContact(contact)} className= 'contact-remove'>
 //                             Remove
 //                         </button>
 //                     </li>
 //                 ))}
 //             </ol>
-//         )
-//     }
+//     )
 // }
+
+class ListContacts extends Component {
+
+    static  propTypes = {
+        contacts: propTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+
+    render() {
+        return (
+            <ol className='contact-list'>
+                {this.props.contacts.map((contact) => (
+                    <li key={contact.id} className='contact-list-item'>
+                        <div className= 'contact-avatar'
+                            style={
+                                {
+                                    backgroundImage: `url(${contact.avatarURL})`
+                                }
+                            }
+                        ></div>
+
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>
+                        </div>
+                        <button className= 'contact-remove'>
+                            Remove
+                        </button>
+                    </li>
+                ))}
+            </ol>
+        )
+    }
+}
+
+// ListContacts.protoTypes = {
+//     contacts: PropTypes.array.isRequired,
+//     onDeleteContact: PropTypes.func.isRequired
+// }
+
+//
 
 export default ListContacts;
