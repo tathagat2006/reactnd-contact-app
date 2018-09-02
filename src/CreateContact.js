c5bc01a95cdf55bc155fcf75b1cfec8c8260ee54
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-09-02 15:03:25 +0530

    form-serialize the input data
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const values = serializeForm(event.target, {hash: true})
+
+        console.log('values', values);
+    }
 
     render() {
         return(
@@ -12,7 +20,7 @@ class CreateContact extends Component {
                     to='/'>
                     Close
                 </Link>
-                <form className='create-contact-form'>
+                <form onSubmit={this.handleSubmit} className='create-contact-form'>
                 <ImageInput
                     className='create-contact-avatar-input'
                     name='avatarURL'
